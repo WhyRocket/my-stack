a534dc4153481b43b22f4bbf48e445b28ca3fdfe
WhyRocket <160044031+WhyRocket@users.noreply.github.com>
2024-04-04 17:29:58 +0500

    Добавил класс MyStack и сделал в нем события

---
+++ src/MyStack/MyStack.cs
@@ -0,0 +1,27 @@
+﻿namespace MyStack;
+
+internal class MyStack<T>
+{
+    public event Action<string> OnCompleted;
+
+    public T? Value
+    {
+        get
+        {
+            return Value;
+        }
+        set
+        {
+            if (value is null)
+            {
+                OnCompleted($"Стэк стал пустым");
+            }
+            else
+            {
+                OnCompleted($"Добавлен новый элемент: {Value}");
+            }
+
+            Value = value;
+        }
+    }
+}
