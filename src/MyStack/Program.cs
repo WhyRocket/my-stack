cd6db2de477a7697c68f668504f45bc926234e3a
WhyRocket <160044031+WhyRocket@users.noreply.github.com>
2024-04-07 13:20:06 +0500

    Что-то понял в мейне

--- src/MyStack/Program.cs
+++ src/MyStack/Program.cs
@@ -1,10 +1,9 @@
-﻿namespace MyStack
+﻿namespace MyStack;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("Hello, World!");
     }
 }
