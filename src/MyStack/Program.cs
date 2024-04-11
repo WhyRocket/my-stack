3fcdbec0182fbe9a5efbfadd584698eb76c1c467
WhyRocket <160044031+WhyRocket@users.noreply.github.com>
2024-04-11 21:57:55 +0500

    Доработал демонстрацию

--- src/MyStack/Program.cs
+++ src/MyStack/Program.cs
@@ -6,43 +6,84 @@ internal class Program
     {
         MyStack<int> stack = new(10);
 
+        // Событие добавления элемента в стек.
         stack.OnPush += (value) => Console.WriteLine($"Добавлен элемент: {value}");
 
+        // Событие удаление элемента из стека.
         stack.OnPop += (value) => Console.WriteLine($"Удален элемент: {value}");
 
+        // Событие опустошения стека.
         stack.OnEmpty += () => Console.WriteLine("СТЭК ПУСТ !!!");
 
+
+        // Демонстрация рабоыт методов
         stack.Push(1);
         stack.Push(2);
         stack.Push(3);
 
+        Console.WriteLine();
+
         Console.WriteLine($"Верхний элемент в стеке: {stack.Peek()}");
 
+        Console.WriteLine();
+
         stack.Pop();
         stack.Pop();
         stack.Pop();
 
-        //// Исключение переполнения стека.
-        //MyStack<int> stack1 = new(2);
+        // Демонстрация работы исключения переполнения стека.
+        Console.WriteLine();
+        MyStack<int> stack1 = new(2);
 
-        //stack1.Push(3);
-        //stack1.Push(4);
-        //stack1.Push(5);
+        try
+        {
+            stack1.Push(3);
+            stack1.Push(4);
+            stack1.Push(5);
+        }
+        catch (InvalidOperationException ex) 
+        {
+            Console.WriteLine($"Ошибка!\nТекст ошибки: {ex.Message}");
+        }
 
-        //// Исключение удаления элемента из пустого стека.
-        //MyStack<int> stack2 = new(3);
+        // Демонстрация работы исключения удаления элемента из пустого стека.
+        Console.WriteLine();
+        MyStack<int> stack2 = new(3);
 
-        //stack2.Pop();
+        try
+        {
+            stack2.Pop();
+        }
+        catch (InvalidOperationException ex)
+        {
+            Console.WriteLine($"Ошибка!\nТекст ошибки: {ex.Message}");
+        }
 
-        //// Исключения объявления стека больше допустимого максимума.
+        // Демонстрация работы исключения объявления стека больше допустимого максимума.
+        Console.WriteLine();
 
-        //MyStack<int> stack3 = new(1000);
+        try
+        {
+            MyStack<int> stack3 = new(1000);
+        }
+        catch(InvalidOperationException ex)
+        {
+            Console.WriteLine($"Ошибка!\nТекст ошибки: {ex.Message}");
+        }
 
-        //// Исключения взятия элемента из пустого стека.
+        // Демонстрация работы исключения взятия элемента из пустого стека.
+        Console.WriteLine();
 
-        //MyStack<int> stack4 = new(3);
+        MyStack<int> stack4 = new(3);
 
-        //stack4.Peek();
+        try
+        {
+            stack4.Peek();
+        }
+        catch (InvalidOperationException ex)
+        {
+            Console.WriteLine($"Ошибка!\nТекст ошибки: {ex.Message}");
+        }
     }
 }
 
