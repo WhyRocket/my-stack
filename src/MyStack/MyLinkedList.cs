7ed05fdbb63c66deabcae61f76f684116c57a51c
WhyRocket <160044031+WhyRocket@users.noreply.github.com>
2024-04-18 18:12:39 +0500

    Создал класс MyLinkedList

---
+++ src/MyStack/MyLinkedList.cs
@@ -0,0 +1,14 @@
+﻿namespace MyStack;
+
+internal class MyLinkedList<T>
+{
+    private sealed class Node<T>(T value)
+    {
+        public T Value { get; set; } = value;
+        public Node<T>? Next { get; set; }
+        public Node<T>? Previous { get; set; }
+    }
+
+    private Node<T>? head;
+    private Node<T>? tail;
+}
