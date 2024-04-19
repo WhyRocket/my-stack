8d3807399de30766e04b6302fa5e3b5c6809c80f
WhyRocket <160044031+WhyRocket@users.noreply.github.com>
2024-04-19 18:49:42 +0500

    Создал класс MyLinkedStack

---
+++ src/MyStack/MyLinkedStack.cs
@@ -0,0 +1,34 @@
+﻿namespace MyStack;
+
+public class MyLinkedStack<T>
+{
+    private class Node(T value)
+    {
+        public T Value { get; set; } = value;
+        public Node? Previous { get; set; }
+    }
+
+    // Верхний элемент стека.
+    private Node? _head;
+
+    // Свойство, которое возварщает количесвто элементов в стеке.
+    public int Size { get; private set; }
+
+    // Метод для добавления элемента.
+    public void Push (T data)
+    {
+
+    }
+
+    // Метод для удаления элемента.
+    public void Pop()
+    {
+
+    }
+
+    // Метод для считывания верхнего элемента.
+    public T Peek ()
+    {
+        return default(T);
+    }
+}
